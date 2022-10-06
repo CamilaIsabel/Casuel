f88c824029611e90ceea5b56dedd84e6790bde62
CamilaIsabel <camilazubillaga123@gmail.com>
2022-10-06 00:21:47 -0500

    CL - last improvments

--- src/app/components/articles/articles.component.ts
+++ src/app/components/articles/articles.component.ts
@@ -29,8 +29,8 @@ export class ArticlesComponent {
       alt: 'table3',
       description:'Fine finishes, wide and resistant',
       details:{
-          description:'',
-          size:'',  
+          description:'Material: oak wood Colour: cream Details: 3 additional drawers',
+          size:'135 X 50 X 40cm',  
           src:'./assets/icons/articles/furniture3view.svg',
       }
     },
@@ -40,8 +40,8 @@ export class ArticlesComponent {
       alt: 'table2',
       description:'Ideal for decorating your home',
       details:{
-        description:'',
-        size:'',
+        description:'Material: Oak Wood, Color: Light Brown, Details: Classic and different',
+        size:'200 X 65 X 50cm',
         src:'./assets/icons/articles/furniture2view.svg',
       }
     
@@ -53,8 +53,8 @@ export class ArticlesComponent {
       alt: 'table4',
       description:'Excellent for multipurpose functions',
       details:{
-        description:'',
-        size:'',
+        description:'Material:Stainless steel, Colour:Black and white, Details: added accessories included',
+        size:'120 X 35 X 80cm',
         src:'./assets/icons/articles/furniture4view.svg',
       }
     },
